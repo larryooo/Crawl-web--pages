8932224ca87b3cdffa3343b8126c95ef315a1c44
rui_li <rui_li@trendmicro.com>
2019-01-17 15:47:24 +0800

    第四个网页

--- spider2.py
+++ spider2.py
@@ -35,12 +35,12 @@ def getResponse(url):
 
 
     # write file
-    if not os.path.exists('/internet-security.txt'):
+    if not os.path.exists('/antivirus-plus-security.txt'):
         content = 'title:'+title_str+'\nsummary_info:'+summary_info_str+'\nmain_content:'+main_content
         print(content)
-        with open("internet-security.txt", 'w') as f:
+        with open("antivirus-plus-security.txt", 'w') as f:
             f.write(content)
 
 
 if __name__ == '__main__':
-    getResponse('https://esupport.trendmicro.com/en-us/home/pages/technical-support/internet-security/home.aspx')
+    getResponse('https://esupport.trendmicro.com/en-us/home/pages/technical-support/antivirus-plus-security/home.aspx')
