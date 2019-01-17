30dec75f3ce1343733ea68962fe02aa9adda4c9b
rui_li <rui_li@trendmicro.com>
2019-01-17 16:07:24 +0800

    第8个网页

--- spider2.py
+++ spider2.py
@@ -20,27 +20,28 @@ def getResponse(url):
         title_str = ''.join(m_tr)
 
     # 摘要
-    summary_info = soup.select('.col-md-6.col-xs-12')
-    summary_info_str = summary_info[1].text
+    summary_info = soup.select('.lwi.iconFeatureNew')
+    summary_info_str = summary_info[0].text
     blank_line = re.compile('\n+')
     summary_info_str = blank_line.sub('', summary_info_str)
     # print(summary_info_str)
 
     # 正文
     main_body = soup.select('.col-xs-12')
-    main_content = main_body[7].text
-    m_str = re.compile('\n+')
-    main_content = m_str.sub('', main_content)
+    main_content = main_body[2].text
+    # print(main_content)
+    # m_str = re.compile('\n+')
+    # main_content = m_str.sub('', main_content)
     # print(main_content)
 
 
     # write file
-    if not os.path.exists('doctor-cleaner.txt'):
+    if not os.path.exists('psp-ps3-ps4-security.txt'):
         content = 'title:'+title_str+'\nsummary_info:'+summary_info_str+'\nmain_content:'+main_content
         print(content)
-        with open("doctor-cleaner.txt", 'w') as f:
+        with open("psp-ps3-ps4-security.txt", 'w') as f:
             f.write(content)
 
 
 if __name__ == '__main__':
-    getResponse('https://esupport.trendmicro.com/en-us/home/pages/technical-support/doctor-cleaner/home.aspx')
+    getResponse('https://esupport.trendmicro.com/en-us/home/pages/technical-support/psp-ps3-ps4-security/home.aspx')
